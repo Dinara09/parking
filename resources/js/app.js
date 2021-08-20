cf9b582f933b999b5d7ab4c077fff6f068d956ee
Dinara <dinaraykupova@gmail.com>
2021-08-20 09:54:06 +0300

    fixed app.js file

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,31 +9,3 @@ window.Vue = require('vue');
 
 Vue.component('create', require('./components/CreateNote.vue').default);
 Vue.component('edit', require('./components/EditNote.vue').default);
-
-// window.onload = function () {
-//     let app = new Vue({
-//         el: '#app',
-//         data: {
-//             name: '',
-//             telNumber: '',
-//             address: '',
-//             sex: '',
-//             brand: '',
-//             model: '',
-//             bodyColor: '',
-//             number: '',
-//             isParkedCar: '',
-//         },
-//         methods: {
-//             onSubmit: function() {
-//                 axios.post('/create/addClient', {name: this.name, telNumber: this.telNumber,
-//                     address: this.address, sex: this.sex, brand: this.brand, model: this.model,
-//                     bodyColor: this.bodyColor, number: this.number, isParkedCar: this.isParkedCar}).then(r => this)
-//                     .catch(error => {
-//                         console.error(error.response.data)
-//                     }).then();
-//
-//             }
-//         }
-//     });
-// }
